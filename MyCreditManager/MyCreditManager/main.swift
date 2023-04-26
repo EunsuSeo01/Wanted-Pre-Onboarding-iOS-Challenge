9a099b21b3eba0f05cae9f6636539d2e80fd1d1d
EunsuSeo01 <eunsu0613@naver.com>
2023-04-27 03:46:11 +0900

    Feat: 성적추가 기능 구현
    성적추가 기능을 구현하고
    Student 구조체의 courses 타입을 구조체에서 딕셔너리로 변경

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -11,13 +11,8 @@ import Foundation
 
 struct Student {
     var name: String
-    var subjects: [Subject]
+    var courses: [String:String]
     var GPA: Double
-}
-
-struct Subject {
-    var name: String
-    var grade: String
 }
 
 // 추가된 학생들 리스트
@@ -43,7 +38,8 @@ while true {
         // 학생삭제
         removeStudent()
     case "3":
-        print("3: 성적추가")
+        // 성적추가
+        addCourseGrade()
     case "4":
         print("4: 성적삭제")
     case "5":
@@ -63,7 +59,7 @@ public func addStudent() {
     if inputName.isEmpty {
         print("입력이 잘못되었습니다. 다시 확인해주세요.")
     } else {
-        let inputStudent = Student(name: inputName, subjects: [], GPA: 0)
+        let inputStudent = Student(name: inputName, courses: [:], GPA: 0)
         
         // 이미 존재하는 학생인 경우
         if !(students.filter({ $0.name == inputName }).isEmpty) {
@@ -86,7 +82,7 @@ public func removeStudent() {
     if inputName.isEmpty {
         print("입력이 잘못되었습니다. 다시 확인해주세요.")
     } else {
-        let inputStudent = Student(name: inputName, subjects: [], GPA: 0)
+        let inputStudent = Student(name: inputName, courses: [:], GPA: 0)
         
         // 목록이 비어있으면 바로 종료
         if students.count == 0 {
@@ -101,6 +97,38 @@ public func removeStudent() {
                 students = students.filter { $0.name != inputName }
                 print("\(inputName) 학생을 삭제하였습니다.")
             }
+        }
+    }
+}
+
+// 3: 성적추가
+public func addCourseGrade() {
+    print("성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.\n입력예) Mickey Swift A+\n만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.")
+    let inputCourseRecord = readLine()!.split(separator: " ")
+    
+    // 잘못된 입력 처리
+    if inputCourseRecord.count != 3 {
+        print("입력이 잘못되었습니다. 다시 확인해주세요.")
+    } else {
+        let studentName = String(inputCourseRecord[0])
+        let courseName = String(inputCourseRecord[1])
+        let courseGrade = String(inputCourseRecord[2])
+        
+        // 받은 이름의 학생이 목록에 없을 경우
+        if students.filter({ $0.name == studentName }).isEmpty {
+            print("\(studentName) 학생을 찾지 못했습니다.")
+        } else {
+            // 학생이 있는 경우 그 학생의 인덱스를 찾는다
+            var index = 0
+            for student in students {
+                if student.name == studentName {
+                    break
+                }
+                index += 1
+            }
+            // 해당하는 학생의 과목 성적 정보 추가(변경)
+            students[index].courses[courseName] = courseGrade
+            print("\(studentName) 학생의 \(courseName) 과목이 \(courseGrade)로 추가(변경)되었습니다.")
         }
     }
 }
